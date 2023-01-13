8411d8932a2d715baf564ffc2ab634f61ac0769c
omarproxt <102703203+omarproxt@users.noreply.github.com>
2023-01-13 04:55:18 +0200

    Update Forum-Information-prox-t.py

--- Forum-Information-prox-t.py
+++ Forum-Information-prox-t.py
@@ -15,6 +15,7 @@ Ya_Bs = '\033[1;34m'
 S_aBs = '\033[1;33m'
 logo = pyfiglet.figlet_format('omarproxt')
 print(a_bSa+logo)
+webbrowser.open('https://t.me/omarproxt25')
 import amino
 client = amino.Client()
 com_id = client.get_from_code(input("-- Community Link••>> ")).json["extensions"]["community"]["ndcId"]
